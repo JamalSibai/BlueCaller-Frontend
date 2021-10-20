68490deb98f288113a8c15a2de01d6551b882005
JamalSibai <Jamal.Sibai.JS@gmail.com>
2021-10-20 17:06:54 +0300

    Updating Profile Design

--- src/screens/UserScreens/Profile.tsx
+++ src/screens/UserScreens/Profile.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { store } from "../../redux/store";
 import { deleteUser } from "../../redux/slices/userSlice";
@@ -22,32 +22,195 @@ export default function Profile({ navigation }) {
   };
 
   return (
-    <View>
+    // <View>
+    //   <TouchableOpacity onPress={onpress}>
+    //     <Text> Edit Image </Text>
+    //   </TouchableOpacity>
+    //   <TouchableOpacity onPress={onpress2}>
+    //     <Text> Edit Name </Text>
+    //   </TouchableOpacity>
+    //   <TouchableOpacity onPress={onpress3}>
+    //     <Text> Edit Phone Number </Text>
+    //   </TouchableOpacity>
+    //   <TouchableOpacity onPress={onpress4}>
+    //     <Text> Edit Password </Text>
+    //   </TouchableOpacity>
+
+    //   <TouchableOpacity onPress={Logout}>
+    //     <Text> Logout </Text>
+    //   </TouchableOpacity>
+    // </View>
+    <View style={{ backgroundColor: "white" }}>
       <TouchableOpacity onPress={onpress}>
-        <Text> Edit Image </Text>
+        <View style={styles.header}></View>
       </TouchableOpacity>
+      <Image
+        style={styles.avatar}
+        source={{
+          uri: "https://bluecaller.tk/storage/Me8inkaENWQGbmCvdXjsbF4ZEAE2dGEVnbgKs8YB.jpg",
+        }}
+      />
       <TouchableOpacity onPress={onpress2}>
-        <Text> Edit Name </Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onpress3}>
-        <Text> Edit Phone Number </Text>
+        <View style={styles.row}>
+          <View>
+            <View style={styles.nameContainer}>
+              <Text
+                style={styles.nameTxtLabel}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Name:
+              </Text>
+              <Text
+                style={styles.nameTxt}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Jamal Sibai
+              </Text>
+            </View>
+          </View>
+        </View>
       </TouchableOpacity>
       <TouchableOpacity onPress={onpress4}>
-        <Text> Edit Password </Text>
+        <View style={styles.row}>
+          <View>
+            <View style={styles.nameContainer}>
+              <Text
+                style={styles.nameTxtLabel}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Email:
+              </Text>
+              <Text
+                style={styles.nameTxt}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                "Jamal@gmail.com"
+              </Text>
+            </View>
+          </View>
+        </View>
       </TouchableOpacity>
-
-      <TouchableOpacity onPress={Logout}>
-        <Text> Logout </Text>
+      <TouchableOpacity onPress={onpress3}>
+        <View style={styles.row}>
+          <View>
+            <View style={styles.nameContainer}>
+              <Text
+                style={styles.nameTxtLabel}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Phone:
+              </Text>
+              <Text
+                style={styles.nameTxt}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                +961 76630304
+              </Text>
+            </View>
+          </View>
+        </View>
       </TouchableOpacity>
+      <View style={{ alignItems: "center", marginTop: 50 }}>
+        <TouchableOpacity style={[styles.buttonContainer, styles.fabookButton]}>
+          <View style={styles.socialButtonContent}>
+            <Text style={styles.loginText}>Logout</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 15,
-    flex: 1,
+  header: {
+    backgroundColor: "#000",
+    height: 200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 40,
+  },
+  row: {
+    // flexDirection: "row",
+    // alignItems: "center",
+    borderColor: "#DCDCDC",
     backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    padding: 10,
+    paddingBottom: 10,
+    // marginLeft: 20,
+  },
+  pic: {
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+  },
+  nameContainer: {
+    // flexDirection: "row",
+    justifyContent: "space-between",
+    width: 280,
+    marginTop: 20,
+  },
+  nameTxtLabel: {
+    marginLeft: 28,
+    fontWeight: "600",
+    color: "#585858",
+    fontSize: 18,
+    width: 170,
+  },
+  nameTxt: {
+    marginLeft: 35,
+    fontWeight: "700",
+    color: "#222",
+    fontSize: 20,
+    width: 300,
+  },
+  mblTxt: {
+    fontWeight: "200",
+    color: "#777",
+    fontSize: 13,
+  },
+  msgContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  msgTxt: {
+    fontWeight: "400",
+    color: "#008B8B",
+    fontSize: 12,
+    marginLeft: 15,
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+  },
+  loginText: {
+    color: "white",
+  },
+  fabookButton: {
+    backgroundColor: "#000",
+  },
+  socialButtonContent: {
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
   },
 });
